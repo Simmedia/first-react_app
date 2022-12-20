9743f6c6b08f9a455ffb19782d80b27400919d7c
Michael Ibrahim <segunmichael24@gmail.com>
2022-12-20 07:06:44 +0100

    Deploy React app to GitHub Pages

---
+++ precache-manifest.b763ccaf8195e6d2edb2102c24f3cda5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "03171ab9ff8b9b6fcd7d",
+    "url": "/first-react_app/static/css/main.237aac0b.chunk.css"
+  },
+  {
+    "revision": "03171ab9ff8b9b6fcd7d",
+    "url": "/first-react_app/static/js/main.9c2c95e8.chunk.js"
+  },
+  {
+    "revision": "2b13b49d10552a0dfe73",
+    "url": "/first-react_app/static/js/runtime~main.bac76c04.js"
+  },
+  {
+    "revision": "4ee2939d001f026f4040",
+    "url": "/first-react_app/static/js/2.10ea7947.chunk.js"
+  },
+  {
+    "revision": "17568128810c26d53c27",
+    "url": "/first-react_app/static/js/3.5e86fb12.chunk.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/first-react_app/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "983124ed0065c116421e4c834a48e726",
+    "url": "/first-react_app/static/media/about-page-img.983124ed.png"
+  },
+  {
+    "revision": "42b3705737c0cb68a1e72b0c344b6878",
+    "url": "/first-react_app/index.html"
+  }
+];
